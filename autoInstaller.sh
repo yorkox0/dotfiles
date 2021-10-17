3d95ef041cd38e7e85a99b0b5ad7fd546f5e4016
yorkox <49209840+yorkox0@users.noreply.github.com>
2021-10-17 21:42:25 +0200

    Update autoInstaller.sh

--- autoInstaller.sh
+++ autoInstaller.sh
@@ -2,6 +2,30 @@
 
 function main(){
 
-  sudo pacman -S bspwm sxhkd polybar 
+  sudo pacman -Syyu bspwm sxhkd polybar papirus-icon-theme xorg ruby rubygems nodejs feh go picom npm firefox gcc qtile pcmanfm rofi neovim nano lightdm lightdm-webkit2-greeter alacritty base-devel make cmake fakeroot neofetch git python3 python2 python-pip alsa-utils binutils cbatticon volumeicon udiskie network-manager-applet lxappearance --noconfirm
+  
+  git clone https://aur.archlinux.org/yay.git
+
+  chmod 777 yay/
+
+  cd yay/
+
+  makepkg -si
+  
+  cd ..
+  
+  sudo rm -r yay/
+  
+  yay -S nerd-fonts-complete --noconfirm
+  
+  pip install pycritty
+  
+  pycritty -s 16 -f Caskaydia -t dracula -o 0.8
+
+  pycritty -p 12 12
+  
+  echo "picom &" >> ~/.xprofile
+  
+  
 
 }
