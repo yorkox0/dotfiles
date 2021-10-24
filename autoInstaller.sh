f1131e0335dc00abcda06d2249358bf7b52d05a0
yorkox <49209840+yorkox0@users.noreply.github.com>
2021-10-24 12:26:38 +0200

    Update autoInstaller.sh

--- autoInstaller.sh
+++ autoInstaller.sh
@@ -49,6 +49,20 @@ function main(){
   
   yay -S arc-solid-gtk-theme --noconfirm
   
+  git clone https://github.com/antoniosarosi/dotfiles
+
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+  cp -r dotfiles/.config/nvim/ ~/.config/
+
+  pip install neovim
+  sudo npm i -g neovim
+  
+  sudo pacman -S neovim
+  
+  sudo rm -r dotfiles/
+  
   #sudo systemctl enable --now lightdm
 
 }
